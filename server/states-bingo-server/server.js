94fa703e8a0e9abea8637e9218e98a26c5c9e5f8
laurance <liulaurance@gmail.com>
2025-04-25 16:02:48 -0700

    updated App.css

--- server/states-bingo-server/server.js
+++ server/states-bingo-server/server.js
@@ -9,17 +9,12 @@ const DATA_FILE = path.join(__dirname, 'board.json');
 
 // All 50 states, excluding Maryland
 const ALL_STATES = [
-  "Alabama","Alaska","Arizona","Arkansas","California","Colorado",
-  "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho",
-  "Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana",
-  "Maine","Maryland","Massachusetts","Michigan","Minnesota",
-  "Mississippi","Missouri","Montana","Nebraska","Nevada",
-  "New Hampshire","New Jersey","New Mexico","New York",
-  "North Carolina","North Dakota","Ohio","Oklahoma","Oregon",
-  "Pennsylvania","Rhode Island","South Carolina","South Dakota",
-  "Tennessee","Texas","Utah","Vermont","Virginia","Washington",
-  "West Virginia","Wisconsin","Wyoming"
-].filter(s => s !== "Maryland");
+  "AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA",
+  "KS","KY","LA","ME","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM",
+  "NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA",
+  "WA","WV","WI","WY"
+];
+
 
 // Fisher–Yates shuffle
 function shuffle(arr) {
